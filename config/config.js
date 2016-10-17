9012bcfc6cf5071c7d31114112ebcd006570007b
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2016-10-17 10:16:54 +0200

    small movements

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {
-    hostname: process.env.HOSTNAME || 'http://localhost:7777/', // for requests
+    hostname: process.env.HOSTNAME || 'http://localhost:8080/', // for requests
     host: process.env.IP || 'localhost',
-    httpPort: process.env.PORT || 7777,
+    httpPort: process.env.PORT || 8080,
     env: process.env.NODE_ENV || 'dev',
     apiToken: process.env.API_TOKEN || 'local123',
     neo4j: process.env.NEO4J_URL || 'http://neo4j:prodyna@localhost:7474',
